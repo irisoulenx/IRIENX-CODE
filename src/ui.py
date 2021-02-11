b5b506886b9a094bb509759579f4befa64de7fcc
Siegfried Wu <siegfried.wu@outlook.at>
2021-02-11 21:04:16 +0800

    Fix a Bug

--- src/ui.py
+++ src/ui.py
@@ -1,25 +1,45 @@
-import wx
-
-class MainWindow(wx.Frame):
-    def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(200,100))
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.CreateStatusBar() # A Statusbar in the bottom of the window
-
-        # Setting up the menu.
-        filemenu= wx.Menu()
-
-        # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
-        filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
-        filemenu.AppendSeparator()
-        filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
-
-        # Creating the menubar.
-        menuBar = wx.MenuBar()
-        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
-        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
-        self.Show(True)
-
-app = wx.App(False)
-frame = MainWindow(None, "Sample editor")
-app.MainLoop()
+import wx 
+ 
+class Mywin(wx.Frame): 
+            
+   def __init__(self, parent, title): 
+      super(Mywin, self).__init__(parent, title = title)  
+      self.InitUI() 
+         
+   def InitUI(self): 
+      menubar = wx.MenuBar() 
+      menu = wx.Menu() 
+      menubar.Append(menu,"File") 
+      self.SetMenuBar(menubar) 
+         
+      tb = wx.ToolBar( self, -1 ) 
+      self.ToolBar = tb 
+         
+      tb.AddTool( 101, wx.Bitmap("new.png") ) 
+      tb.AddTool(102,wx.Bitmap("save.png")) 
+         
+      right = tb.AddRadioTool(222,wx.Bitmap("right.png")) 
+      center = tb.AddRadioTool(333,wx.Bitmap("center.png")) 
+      justify = tb.AddRadioTool(444,wx.Bitmap("justify.png"))
+		
+      tb.Bind(wx.EVT_TOOL, self.Onright)
+      tb.Bind(wx.EVT_COMBOBOX,self.OnCombo) 
+      self.combo = wx.ComboBox( tb, 555, value = "Times", choices = ["Arial","Times","Courier"])  
+		
+      tb.AddControl(self.combo ) 
+      tb.Realize() 
+      self.SetSize((350, 250)) 
+              
+      self.text = wx.TextCtrl(self,-1, style = wx.EXPAND|wx.TE_MULTILINE) 
+      self.Centre() 
+      self.Show(True) 
+		
+   def Onright(self, event): 
+      self.text.AppendText(str(event.GetId())+"\n")
+		
+   def OnCombo(self,event): 
+      self.text.AppendText( self.combo.GetValue()+"\n")  
+		
+ex = wx.App() 
+Mywin(None,'ToolBar Demo - www.yiibai.com') 
+ex.MainLoop()
